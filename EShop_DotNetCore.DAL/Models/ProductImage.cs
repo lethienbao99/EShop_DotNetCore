8186f81f28fed6317cd48f8986b4cd3c963e0828
lethienbao99 <1754050012bao@ou.edu.vn>
2020-09-05 20:59:48 +0700

    Create table ProductImage

---
+++ EShop_DotNetCore.DAL/Models/ProductImage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EShop_DotNetCore.DAL.Models
+{
+    public class ProductImage
+    {
+        public int ProductImageId { get; set; }
+        public int ProductId { get; set; }
+        public string Path { get; set; }
+        public string Alt { get; set; }
+        public bool IsDefault { get; set; }
+        public DateTime DateCreated { get; set; }
+        public int SortOrder { get; set; }
+        public Product Product { get; set; }
+    }
+}
